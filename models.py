7e128c85bc7dc52390054fc01eb4d20aa850c38c
adejolaO <aogunsan@cmitsouth.org>
2026-03-08 20:32:32 -0400

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,9 @@
+"""
+SUMMARY OF CANDIDATE CLASSES
+
+1. **Customer**: Represents the users of the ByteBites app, including their profiles, preferences, and interactions with the app.
+2. **Transaction**: Represents the transaction details of the customers, including purchase history, payment methods, and transaction status.
+3. **MenuItem**: Represents the items available on the ByteBites menu.
+4. **FoodItem**: Represents the individual food items availabel for customers.
+
+"""
